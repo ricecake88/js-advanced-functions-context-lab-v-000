512f26f8c6f57c820121201e3666b9d3ffe725da
Grace Shih <vocalise@gmail.com>
2020-02-13 05:24:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,5 +57,5 @@ let hoursWorkedOnDate = function(date) {
 }
 
 function wagesEarnedOnDate(date) {
-  return hoursWorkedOnDate.call(date);
+  return hoursWorkedOnDate.call(createEmployeeRecord, date);
 }
